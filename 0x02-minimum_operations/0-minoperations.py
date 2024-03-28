15bd0c1771229821be4a8f5b0442cf61fd1bfa9a
esianyo <esianicd@gmail.com>
2024-03-28 22:53:47 +0000

    min ops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,7 +6,8 @@ Minimum Operations
 
 def minOperations(n):
     """
-    Calculates the minimum number of copy and paste operations needed to reach n characters.
+    Calculates the minimum number of copy
+    and paste operations needed to reach n characters.
 
     Args:
         n: The target number of characters.
@@ -19,7 +20,7 @@ def minOperations(n):
     curr_len = 1  # Start with 1 character
 
     while curr_len < n:
-        # Find the largest power of 2 that's less than or equal to the difference
+        # Find largest power of 2 less than or equal to difference
         diff = n - curr_len
         largest_power_of_2 = 1
         while largest_power_of_2 * 2 <= diff:
