5056211d978263e34014443b11d31ceb91181a4e
esianyo <esianicd@gmail.com>
2024-05-01 17:27:37 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,7 +17,9 @@ def makeChange(coins, total):
 
     for amount in range(1, total + 1):
         for coin in coins:
-            if coin <= amount:
+            if coin > total:
+                pass
+            elif coin <= amount:
                 dynamicProg[amount] = min(dynamicProg[amount],
                                           dynamicProg[amount - coin] + 1)
 
